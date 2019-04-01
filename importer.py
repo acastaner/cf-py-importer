496ea1f727defc3a164e0945370e1b0186e74921
Arnaud Castaner <arnaud.castaner@protonmail.com>
2019-04-01 14:28:22 +0200

    Update to use cyberfloodClient package

--- importer.py
+++ importer.py
@@ -10,12 +10,14 @@
 # This program is NOT officially supported by Spirent
 
 from config import *
-from lib.models.CfClient import CfClient
+#from lib.models.CfClient import CfClient
 from lib import importerLib
 from lib.models.Scenario import Scenario, ScenarioType
 
 import sys
 import os
+
+import cyberfloodClient
 
 # temp
 import json
@@ -26,10 +28,10 @@ import json
 
 print("Welcome to the CyberFlood Mass PCAP Importer!")
 print("Checking for connectivity & credentials...", end=" ")
-cfClient = CfClient(globalSettings["userName"],
-                    globalSettings["userPassword"],
-                    globalSettings["cfControllerAddress"]
-                    )
+cfClient = cyberfloodClient.CfClient(globalSettings["userName"],
+                                     globalSettings["userPassword"],
+                                     globalSettings["cfControllerAddress"]
+                                     )
 cfClient.generateToken()
 if cfClient.isLogged():
     print("success! [" + cfClient.userName + "]")
@@ -37,7 +39,8 @@ else:
     print("error! Please check your configuration.")
     sys.exit()
 print("Looking for PCAPs to import...")
-attacks = importerLib.getPcapFiles(os.path.join('.', 'content', 'to_process', 'attacks'), ScenarioType.ATTACK)
+attacks = importerLib.getPcapFiles(os.path.join(
+    '.', 'content', 'to_process', 'attacks'), ScenarioType.ATTACK)
 applications = importerLib.getPcapFiles(
     os.path.join('.', 'content', 'processed', 'applications'), ScenarioType.APPLICATION)
 print("\tAttacks: " + str(attacks.__len__()))
